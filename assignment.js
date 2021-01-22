310196b685880a65e026e9e95dc0bfbd09871e03
shajidulashfak <shajidulashfak@gmail.com>
2021-01-22 20:03:55 +0600

    done

--- assignment.js
+++ assignment.js
@@ -7,20 +7,20 @@ function kilometerToMeter(kilometer){
 }
 
 
-// var watchPrice = 50;
-// var phonePrice = 100;
-// var laptopPrice = 500;
+var watchPrice = 50;
+var phonePrice = 100;
+var laptopPrice = 500;
          
 
-//  function budgetCalculator(watch, phone, laptop){
-//         var watchTotal = watch * 50;
-//         var phoneTotal = phone * 100;
-//         var laptopTotal = laptop * 500;
+ function budgetCalculator(watch, phone, laptop){
+        var watchTotal = watch * 50;
+        var phoneTotal = phone * 100;
+        var laptopTotal = laptop * 500;
         
-//         return watchTotal + phoneTotal + laptopTotal;     
+        return watchTotal + phoneTotal + laptopTotal;     
         
         
-//  }
+ }
  
 
 // No:3. Hotel Cost ;
